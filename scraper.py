1b225416a62835ffa1abd99a9ba91d73b6557706
Pratap512 <hpsr512@gmail.com>
2024-09-14 20:55:56 +0530

    added region search functionality

--- scraper.py
+++ scraper.py
@@ -1,17 +1,55 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
 import csv
 import time
+
+
+def set_search_filters(driver):
+    try:
+        try:
+            cookie_reject_button = WebDriverWait(driver, 10).until(
+                EC.element_to_be_clickable((By.ID, 'onetrust-reject-all-handler'))
+            )
+            cookie_reject_button.click()
+            print("Cookie consent rejected")
+        except:
+            print("Cookie consent not found or already handled")
+
+        driver.implicitly_wait(10)
+        open_button = WebDriverWait(driver, 50).until(
+            EC.element_to_be_clickable((By.XPATH, '//*[@id="__APP"]/div[2]/main/div[2]/div[2]/div/div[2]/div[2]/div[1]/div[1]/div/div[1]/div/div/div'))
+        )
+        open_button.click()
+        driver.implicitly_wait(10)
+
+        search_box = WebDriverWait(driver, 50).until(
+            EC.element_to_be_clickable((By.XPATH, '//*[@id="__APP"]/div[2]/main/div[2]/div[2]/div/div[2]/div[2]/div[1]/div[1]/div/div[2]/div/div/div[1]/div/input'))
+        )
+        search_box.send_keys("India")
+
+        india_button = WebDriverWait(driver, 50).until(
+            EC.element_to_be_clickable((By.XPATH, '//*[@id="__APP"]/div[2]/main/div[2]/div[2]/div/div[2]/div[2]/div[1]/div[1]/div/div[2]/div/div/div[2]/div[2]'))
+        )
+        india_button.click()
+
+        time.sleep(5)
+        driver.implicitly_wait(10)
+    except Exception as e:
+        raise e
+
 
 service = Service(ChromeDriverManager().install())
 driver = webdriver.Chrome(service=service)
 
 url = "https://p2p.binance.com/en/trade/all-payments/USDT?fiat=INR"
-
 driver.get(url)
 time.sleep(5)
+
+set_search_filters(driver)
 
 csv_filename = "binance_trade_data.csv"
 headers = ["Trader Name", "Price", "Available", "Order Limit", "Completion Percentage", "Payment Modes"]
